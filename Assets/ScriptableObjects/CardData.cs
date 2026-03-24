b14a69d794ed22e07b84267415f3c1e846987fe0
Carla Pallés Ros <carlaa.pr15@gmail.com>
2026-03-24 19:56:51 +0100

    Scriptable Objects

---
+++ Assets/ScriptableObjects/CardData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Item/Card", order = 1)]
+public class CardData : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    [Range(0, 50)]
+    public int Mana;
+    public int Attack;
+    public int Health;
+    public Sprite Image;
+
+    public void Print()
+    {
+        Debug.Log(Name + " costs: " + Mana + " Mana.");
+    }
+}
---
+++ Assets/ScriptableObjects/CardUI.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardUI : MonoBehaviour
+{
+    public CardData CardItem;
+
+    public TextMeshProUGUI TextName;
+    public TextMeshProUGUI TextDescription;
+    public TextMeshProUGUI TextMana;
+    public TextMeshProUGUI TextAttack;
+    public TextMeshProUGUI TextHealth;
+    public Image Image;
+
+    void Start()
+    {
+        SetCard(CardItem);
+    }
+
+    void SetCard(CardData cardItem)
+    {
+        TextName.text = cardItem.Name;
+        TextDescription.text = cardItem.Description;
+        TextMana.text = cardItem.Mana.ToString();
+        TextAttack.text = cardItem.Attack.ToString();
+        TextHealth.text = cardItem.Health.ToString();
+        Image.sprite = cardItem.Image;
+    }
+}
